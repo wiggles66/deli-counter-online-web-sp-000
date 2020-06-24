80ea82f1a9620efcef59c3f121a348f030f2393c
Michael Waide <mvwaide@gmail.com>
2020-06-24 16:29:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 katz_deli = ["Moshe", "Fayge", "Rivki"]
 
 
-def line(array) # this was the one I figured out
+def line(array)
   if array.length >= 1
     nuarray = []
     counter = 1
@@ -17,36 +17,31 @@ end
 
 line(katz_deli)
 
-def line_simple(array) # this one follows the same mechanics as learn.co
+def line_simple(array)
   current_line = "The simple line is currently:"
   array.each.with_index(1) do |value, indexemus|
-  # "each.with_index" is the method...must use "index"
+
     current_line << " #{indexemus}. #{value},"
-    # "indexemus" is used to illustrate this variable can be different from "index"
+
   end
   puts current_line
 end
 
 line_simple(katz_deli)
-#[:foo, :bar, :baz].each.with_index(1) do |value, index|
-#    puts "#{index}: #{value}"
-#end
-
-
 
 def take_a_number(line, new_person)
-  line.push(new_person) # could say: "line << new_person"
+  line.push(new_person)
   puts "Welcome, #{new_person}. You are number #{line.length} in line."
 end
 
 take_a_number(katz_deli, "Fyvish")
 
 def now_serving(line)
-  if line.length == 0 # could say: "if deli.empty?"
+  if line.length == 0
     puts"There is nobody waiting to be served!"
   else
-    puts "Currently serving #{line[0]}." # could say: "Currently serving #{line.first}."
-    line.shift # this works in the IDE but no on repl.it
+    puts "Currently serving #{line[0]}."
+    line.shift
   end
 end
 
